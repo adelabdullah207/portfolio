ad0b38ac9cde12c40fc4a120e152b1d6d83e69c5
adelabdullah207 <vitothedev207@gmail.com>
2025-04-17 20:43:51 +0200

    Updates

---
+++ assets/Footer-Ts5Mv82l.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-b3oSKnIK.js";function r(){return t.jsx("div",{className:"bg-[#040428] text-white text-center p-2",children:"created with ♥ by adel abdullah"})}export{r as default};
