a65f1b738ad1b10c8436f1dae635147c535bb000
adelabdullah207 <vitothedev207@gmail.com>
2025-04-28 22:57:02 +0400

    Updates

--- assets/Footer-BUET4pUe.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-heXpi58C.js";function r(){return t.jsx("div",{className:"bg-[#040428] text-white text-center p-2",children:"created with ♥ by adel abdullah"})}export{r as default};
