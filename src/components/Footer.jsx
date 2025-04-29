434d3c9691ae2150e36cd33053715663ffd12486
adelabdullah207 <vitothedev207@gmail.com>
2025-04-29 14:07:28 +0400

    vito

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from "react";
 
 export default function Footer() {
-  const [date, setDate] = useState();
-  const currentYear = new Date().getFullYear().toString();
   useEffect(() => {
     setDate(currentYear);
   });
   return (
     <div className="bg-[#040428] text-white text-center p-4 capitalize font-bold">
-      created with{" "}
-      <span className=" animate-pulse text-red-500">&hearts; </span>
-      by adel abdullah <span>{date}</span>
+      created by <span className=" animate-pulse text-red-500">&hearts; </span>
+      by adel abdullah
     </div>
   );
 }
